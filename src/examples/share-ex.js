26354a2021e3b179af7515c65fcca28e788acd3f
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-04-16 21:53:53 +0300

    Add the example of using `Share`

---
+++ src/examples/share-ex.js
@@ -0,0 +1,20 @@
+const URLS = require("./test-urls-private");
+const {util} = require("../util");
+const {Nodes} = require("../nodes");
+const Share = require("../share");
+
+async function example() {
+    const url = URLS.CAT_FOLDER_NO_KEY_SELECTED_FOLDER_2;
+    const share = Share.fromUrl(url);
+    console.log(url);
+    console.log(share.getUrl(true));
+    console.log();
+    console.log(share.toString());
+}
+
+
+!async function main() {
+    console.time("Time");
+    await example();
+    console.timeEnd("Time");
+}();
