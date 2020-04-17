cead1a2e69c463ac85e5631f3c7ab67232f6c1c2
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-04-17 20:49:58 +0300

    Fix `node-fetch` import

--- src/browser-context.js
+++ src/browser-context.js
@@ -1,6 +1,6 @@
 const btoa = str => Buffer.from(str, "binary").toString("base64");
 const atob = b64 => Buffer.from(b64, "base64").toString("binary");
 
-const fetch = require("node-fetch");
+const fetch = require("node-fetch").default;
 
 module.exports = { btoa, atob, fetch };
