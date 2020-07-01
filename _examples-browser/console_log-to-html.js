d8efd364a2d31676dafdde1a06198ecc8b79c323
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-07-01 16:22:05 +0300

    Update console_log-to-html.js

--- _examples-browser/console_log-to-html.js
+++ _examples-browser/console_log-to-html.js
@@ -1,24 +1,32 @@
-globalThis.log = console.log;
-function htmlLog() {
+console.log = getHtmlLog(console.log);
+console.warn = getHtmlLog(console.warn, "warning");
+console.error = getHtmlLog(console.error, "error");
 
-    log(...arguments);
+function getHtmlLog(func, className) {
+    globalThis[func.name] = func;
+    return function() {
+            func(...arguments);
 
-    const entry = document.createElement("div");
-    entry.classList.add("console-entry");
+            const entry = document.createElement("div");
+            entry.classList.add("console-entry");
+            if (className) {
+                entry.classList.add(className);
+            }
 
-    for (const argument of arguments) {
+            for (const argument of arguments) {
 
-        const str = toString(argument);
+                const str = toString(argument);
 
-        const block = document.createElement("pre");
-        block.textContent = str;
-        entry.append(block);
-    }
+                const block = document.createElement("pre");
+                block.textContent = str;
+                entry.append(block);
+            }
 
-    const root = document.querySelector("#console_log-to-html") || document.querySelector("body");
+            const root = document.querySelector("#console_log-to-html") || document.querySelector("body");
 
-    root.append(entry);
-    root.append(document.createElement("hr"));
+            root.append(entry);
+            root.append(document.createElement("hr"));
+    };
 }
 
 function toString(argument) {
@@ -64,5 +72,3 @@ function toString(argument) {
         console.error(e);
     }
 }
-
-console.log = htmlLog;
